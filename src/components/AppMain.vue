bb9bfcc554724ebc76b2b190077b2aedc4b83449
AlesciDavide <davi.alesci@email.com>
2024-06-14 11:30:38 +0200

    milestone 1

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -9,7 +9,7 @@ export default{
     },
     methods:{
         getFilms(){
-            axios.get('https:/api.themoviedb.org/3/search/movie?api_key=1231079dd3a1fe954db3fe98b67aa52f&language=it-IT&query=ritorno al futuro').then(response => {
+            axios.get('https:/api.themoviedb.org/3/search/movie?api_key=1231079dd3a1fe954db3fe98b67aa52f&language=it-IT&query=' + store.searchFilm).then(response => {
                 this.store.films = response.data.results;
                 console.log(response.data.results);
                 
@@ -32,6 +32,8 @@ export default{
 </script>
 
 <template>
+    <input type="text" placeholder="Cerca un film" v-model="store.searchFilm" @keyup.enter="getFilms">
+    <button @click="getFilms">Cerca</button>
     <ul>
         <li v-for="film in store.films">
             {{film.title}} <br>
