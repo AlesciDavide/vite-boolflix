8be2e15d581ecdd50d74d30e9599da5bc8aa359e
AlesciDavide <davi.alesci@email.com>
2024-06-18 18:51:34 +0200

    aggiunta serie tv di tendenza al primo avvio

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -32,6 +32,19 @@ export default{
             .finally(function () {
                 // always executed
             });
+            axios.get('https://api.themoviedb.org/3/trending/tv/day?api_key=1231079dd3a1fe954db3fe98b67aa52f&language=it-IT').then(response => {
+                
+                this.store.series = response.data.results;
+                console.log(response.data.results);
+                
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .finally(function () {
+                // always executed
+            });
         },
     },
     created(){
